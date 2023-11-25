177974bf7a4f198a00ce13ab9630a1039be37e6b
dokka-bae <dovidovich0460@gmail.com>
2023-11-25 20:17:18 +0300

    seventh task

--- lesson_12_prob/task7.py
+++ lesson_12_prob/task7.py
@@ -1,4 +1,23 @@
-# Вероятность того, что лампочка перегорит в течение первого дня эксплуатации, равна 0.0004. 
-# В жилом комплексе после ремонта в один день включили 5000 новых лампочек. 
-# Какова вероятность, что ни одна из них не перегорит в первый день? 
+# Вероятность того, что лампочка перегорит в течение первого дня эксплуатации, равна 0.0004.
+# В жилом комплексе после ремонта в один день включили 5000 новых лампочек.
+# Какова вероятность, что ни одна из них не перегорит в первый день?
 # Какова вероятность, что перегорят ровно две?
+import numpy as np
+from scipy import stats
+
+
+class Handler:
+    def __init__(self, numbers: np.int64, predict_numbers: np.int64) -> None:
+        self.numbers = numbers
+        self.predict_numbers = predict_numbers
+        self.run()
+
+    def run(self):
+        print("Ни одна лампа не перегорит:", stats.poisson.pmf(
+            self.predict_numbers[0], self.numbers * 0.0004))
+        print("Две лампы сгорят:", stats.poisson.pmf(
+            self.predict_numbers[1], self.numbers * 0.0004))
+
+
+if __name__ == '__main__':
+    Handler(numbers=5000, predict_numbers=[0, 2])
