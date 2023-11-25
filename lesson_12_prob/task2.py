40a09bafab2cdb2061b54fa26ca25099ac16f56c
dokka-bae <dovidovich0460@gmail.com>
2023-11-25 19:08:54 +0300

    second task

--- lesson_12_prob/task2.py
+++ lesson_12_prob/task2.py
@@ -1,2 +1,24 @@
-# Случайная непрерывная величина A имеет равномерное распределение на промежутке (200, 800]. 
+# Случайная непрерывная величина A имеет равномерное распределение на промежутке (200, 800].
 # Найдите ее среднее значение и дисперсию.
+import numpy as np
+from scipy import stats
+
+
+class Handler():
+    def __init__(self, start: np.int64, end: np.int64) -> None:
+        self.start: np.int64 = start
+        self.end: np.int64 = end
+        self.run()
+
+    def mean(self) -> np.float64:
+        return (self.start+1+self.end)/12
+
+    def variance(self) -> np.float64:
+        return (self.end-self.start-1)**2/12
+
+    def run(self) -> None:
+        print(self.mean(), self.variance)
+
+
+if __name__ == '__main__':
+    Handler(start=200, end=800)
