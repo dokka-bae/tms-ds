a5956ed074260a48625b5b9d4cfd12abdd1194a1
dokka-bae <dovidovich0460@gmail.com>
2023-12-11 23:30:34 +0300

    15 task

--- lesson_15_ml/LinearRegression.py
+++ lesson_15_ml/LinearRegression.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class LinearRegression:
     def __init__(
         self,
@@ -9,48 +8,81 @@ class LinearRegression:
         y: np.ndarray,
         optimizer: str = "SGD",
         loss_function: str = "MSE",
-        learning_rate: np.float64 = 0.01,
-        epochs: int = 100,
+        learning_rate: np.float64 = 0.0005,
+        epochs: int = 5000,
         batch_size: int = 1,
     ) -> None:
         """
-            optimizers: GD - gradient descent, SGD - stochastic gradient descent, MNSGD - mini-batch stochastic gradient
-            loss_functions: MSE - mean squad error, MAE - mean absolute error
+        optimizers: GD - gradient descent, SGD - stochastic gradient descent, BGD - batch gradient descent
+        loss_functions: MSE - mean squared error, MAE - mean absolute error
         """
-        self.__weigths: np.ndarray = np.random.rand(x_train.shape[1])
-        self.__x_train: np.ndarray = x_train
+        self.__x_train: np.ndarray = np.hstack([x_train, np.ones((x_train.shape[0], 1))])
+        self.__weights: np.ndarray = np.zeros((self.__x_train.shape[1], 1))
         self.__y_train: np.ndarray = y_train
         self.__y: np.ndarray = y
-        self.__optimazer: str = optimizer
+        self.__optimizer: str = optimizer
         self.__loss_function: str = loss_function
         self.__learning_rate: np.float64 = learning_rate
         self.__epochs: np.int64 = epochs
         self.__batch_size: np.int64 = batch_size
+        self.__set_loss_function_and_optimizer()
         self.__run()
 
+    def __set_loss_function_and_optimizer(self) -> None:
+        if self.__loss_function == 'MSE':
+            self.__loss_function = self.__mse
+            self.__loss_function_directive = self.__mse_directive
+        elif self.__loss_function == 'MAE':
+            self.__loss_function = self.__mae
+            self.__loss_function_directive = self.__mae_directive
+        else:
+            raise TypeError('Invalid loss function')
+        
+        if self.__optimizer == 'GD':
+            self.__optimizer = self.__gradient_descent
+        elif self.__optimizer == 'SGD':
+            self.__optimizer = self.__stochastic_gradient_descent
+        elif self.__optimizer == 'BGD':
+            self.__optimizer = self.__batch_gradient_descent
+        else:
+            raise TypeError('Invalid optimizer')
+
     def __run(self) -> None:
-        pass
+        self.__train()
 
     def __train(self) -> None:
-        pass
+        for i in range(self.__epochs):
+            gradient = self.__optimizer().reshape(self.__weights.shape)
+            self.__weights -= gradient * self.__learning_rate
+            if (i + 1) % 10 == 0:
+                print(f"Epoch {i + 1} loss_function: {self.__loss_function()}")
 
-    def __gradient_descent(self) -> None:
-        pass
+    def __gradient_descent(self) -> np.ndarray:
+        return -2 / self.__x_train.shape[0] * np.sum((self.__y_train - self.__predict()) * self.__x_train, axis=0)
 
-    def __stochastic_gradient_descent(self) -> None:
-        pass
+    def __stochastic_gradient_descent(self) -> np.ndarray:
+        index = np.random.randint(0, 5)
+        x = self.__x_train[index:index + 1]
+        y = self.__y_train[index:index + 1]
+        return 2 * x.T.dot(x.dot(self.__weights) - y)
 
-    def __mini_batches_stochastic_gradient_descent(self, batch_size=1) -> None:
-        pass
+    def __batch_gradient_descent(self) -> np.ndarray:
+        indices = np.random.choice(len(self.__x_train), size=self.__batch_size, replace=False)
+        return -2 / self.__x_train[indices].shape[0] * np.sum(
+            (self.__y_train[indices] - self.__predict()) * self.__x_train[indices], axis=0
+        )
 
-    def __mse(self, y_pred: np.ndarray) -> np.float64:
-        return np.mean(np.sum((y_pred - self.__y) ** 2))
+    def __predict(self) -> np.ndarray:
+        return self.__x_train.dot(self.__weights)
 
-    def __mae(self, y_pred: np.ndarray) -> np.float64:
-        return np.mean(np.sum(np.abs(y_pred - self.__y)))
+    def __mse(self) -> np.float64:
+        return np.mean((self.__y_train - self.__predict()) ** 2)
 
-    def __mse_directive(self, y_pred: np.ndarray) -> np.float64:
-        return -2 * np.mean(np.sum(self.__x_train - self.__y))
+    def __mae(self) -> np.float64:
+        return np.mean(np.abs(self.__predict() - self.__y_train))
 
-    def __mae_directive(self, y_pred: np.ndarray) -> np.float64:
-        pass
+    def __mse_directive(self) -> np.ndarray:
+        return -2 * np.mean(self.__x_train.T.dot(self.__y_train - self.__predict()))
+
+    def __mae_directive(self) -> np.ndarray:
+        return -np.sign(self.__y_train - self.__predict()).dot(self.__x_train)
--- lesson_15_ml/main.py
+++ lesson_15_ml/main.py
@@ -1,7 +1,18 @@
 from LinearRegression import LinearRegression
+from LinearRegression import np
 
 def main():
-    LinearRegression()
+    x_train = np.array([1,2,3,4,5]).reshape(5,-1)
+    y_train = np.array([2,3,4,5,6]).reshape(5,-1)
+    
+    # np.random.seed(33)
+    # x_train = np.random.rand(1000,1)
+    # x_train = x_train.reshape((-1, 5))
+    # params = np.random.rand(5,1)
+    # y_train = x_train.dot(params) + 2 + np.random.rand(1)
+
+
+    LinearRegression(x_train,y_train,[])
 
 
 if __name__ == '__main__':
